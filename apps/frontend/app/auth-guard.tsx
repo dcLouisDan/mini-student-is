3e44c84e2cf0c531540e97a562bb305d2c528b88
Dan Louis Dela Cruz <louisdc002@gmail.com>
2026-02-26 22:35:04 +0800

    Display auth user info in sidebar and add redirects to auth routes in case of authenticated user

--- apps/frontend/app/auth-guard.tsx
+++ apps/frontend/app/auth-guard.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation'
 import { ReactNode, useEffect } from 'react'
 
 export default function AuthGuard({ children }: { children: ReactNode }) {
-  const { user, isAuthenticated, isLoading } = useAuth()
+  const { isAuthenticated, isLoading } = useAuth()
   const router = useRouter()
 
   useEffect(() => {
@@ -14,8 +14,24 @@ export default function AuthGuard({ children }: { children: ReactNode }) {
       console.log('UNAUTHORIZED')
       router.replace('/auth/login')
     }
+  }, [isAuthenticated, isLoading])
 
-    console.log('USER:', user)
+  if (isLoading) {
+    return <LoadingState />
+  }
+
+  return <>{children}</>
+}
+
+export function UnAuthGuard({ children }: { children: ReactNode }) {
+  const { isAuthenticated, isLoading } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (isAuthenticated && !isLoading) {
+      console.log('AUTHENTICATED')
+      router.replace('/dashboard')
+    }
   }, [isAuthenticated, isLoading])
 
   if (isLoading) {
--- apps/frontend/components/nav-user.tsx
+++ apps/frontend/components/nav-user.tsx
@@ -20,17 +20,9 @@ import {
 } from '@/components/ui/sidebar'
 import useAuth from '@/hooks/use-auth'
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}) {
+export function NavUser() {
   const { isMobile } = useSidebar()
-  const { logout } = useAuth()
+  const { user, logout } = useAuth()
 
   return (
     <SidebarMenu>
@@ -42,12 +34,11 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg">{user?.initials}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">{user?.fullName}</span>
+                <span className="truncate text-xs">{user?.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -61,22 +52,14 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">{user?.initials}</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">{user?.fullName}</span>
+                  <span className="truncate text-xs">{user?.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
